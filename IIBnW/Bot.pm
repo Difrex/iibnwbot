4ff84f627bdc9579c129152fa34fd2a6dff1fc39
Difrex(Denis Zheleztsov) <difrex.punk@gmail.com>
2014-08-11 00:39:27 +0400

    Example code from https://metacpan.org/source/HACKER/Net-XMPP-1.02/examples/client.pl

--- IIBnW/Bot.pm
+++ IIBnW/Bot.pm
@@ -13,18 +13,16 @@ sub new {
     my $config = shift;
     my $bot    = Net::Jabber::Bot->new(
         server              => $config->{server}->{HOST},
-        conference_server   => 'conference.' . $config->{server}->{HOST},
         port                => $config->{server}->{PORT},
         username            => $config->{auth}->{USER},
         password            => $config->{auth}->{PASS},
-        safety_mode         => 1,
-        message_function    => IIBnW::Bot->new_bot_message(),
-        background_function => IIBnW::Bot->background_checks(),
     );
     my $self = { _config => shift, };
 
     bless $self, $class;
     return $self;
 }
+
+
 
 1;
